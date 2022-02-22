f642a7f833eee73fdcc1462ee9d1d3aa43ef98e9
Jesse Toivanen <jesse.toivanen2@gmail.com>
2022-02-22 11:31:55 +0200

    3.3: puhelinluettelon backend step3 tehty.

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -32,15 +32,29 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 app.get('/info', (req, res) => {
     let timestamp = new Date()
-    res.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
+    res.send(`
+        <p>
+            Phonebook has info for ${persons.length} people
+        </p>
         <p>
             ${timestamp.toDateString()}
             ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}
             GMT+0200 (Eastern European Standard Time)
-        </p>`)
+        </p>
+    `)
 })
 
 const PORT = 3001
