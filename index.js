a63c15373191cf60e3f54a6a862d1e53cb3ba4fd
Jesse Toivanen <jesse.toivanen2@gmail.com>
2022-03-02 17:20:17 +0200

    3.19 tehty

--- index.js
+++ index.js
@@ -7,127 +7,112 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.static('build'))
 app.use(express.json())
+
 app.use(cors())
+
+app.use(express.static('build'))
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
-        .then(persons => {
-            res.json(persons)
-        })
-        .catch(error => next(error))
+  Person.find({})
+   .then(persons => {
+      res.json(persons)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+  })
 
 app.get('/info', (req, res, next) => {
-    let timestamp = new Date()
+  let timestamp = new Date()
 
-    Person.countDocuments()
-        .then(count => {
-            res.send(`
-                <p>
-                    Phonebook has info for ${count} people
-                </p>
-                <p>
-                    ${timestamp.toDateString()}
-                    ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}
-                    GMT+0200 (Eastern European Standard Time)
-                </p>
-            `)
-        })
-        .catch(error => next(error))
+  Person.countDocuments()
+    .then(count => {
+      res.send(`
+        <p>
+          Phonebook has info for ${count} people
+        </p>
+        <p>
+          ${timestamp.toDateString()}
+          ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}
+          GMT+0200 (Eastern European Standard Time)
+        </p>
+      `)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if (!body.name) {
-        return res.status(400).json({
-            error: 'name missing'
-        })
-    } else if (!body.number) {
-        return res.status(400).json({
-            error: 'number missing'
-        })
-    }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
     })
-
-    person.save()
-        .then(savedPerson => {
-            res.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+  const { name, number } = req.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
-})
-
-const personCount = Person.countDocuments({}, (error, count) => {
-    if(error) {
-        next(error)
-    } else {
-        return count
-    }
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    }
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
